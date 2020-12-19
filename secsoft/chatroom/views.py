df3200371a51e3f61266b7359ad7aa1e91fd8399
sinisaarinen <sini.saarinen@hotmail.fi>
2020-12-19 15:05:04 +0200

    created index.html

--- secsoft/chatroom/views.py
+++ secsoft/chatroom/views.py
@@ -1,5 +1,6 @@
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Welcome to the chatroom.")
+    return render(request, 'index.html')
